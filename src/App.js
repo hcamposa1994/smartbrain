3cbae11f78fb3f5441eea8bf80d038f7a4dcff3b
Herbert Campos <wohoforever@yahoo.com>
2020-12-27 14:49:38 -0800

    edited Register.js to now ensure an empty user does not go through. Edited App.js to remove the Clarifai API from the front end into the backend to ensure API key was not displayed, removed .env file as is was no longer needed

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ import Signin from "./components/Signin/Signin";
 import Register from "./components/Register/Register";
 
 import Particles from "react-particles-js";
-import Clarifai from "clarifai";
 import "./App.css";
-
-const app = new Clarifai.App({
-  apiKey: process.env.REACT_APP_CLARIFAI_API_KEY,
-});
 
 const particlesOptions = {
   particles: {
@@ -83,8 +78,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch("http://localhost:3000/imageurl", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json())
       .then((response) => {
         if (response) {
           fetch("http://localhost:3000/image", {
